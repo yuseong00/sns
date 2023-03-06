ca8e38efe76d39f6cf0e6ba5b60b0d5c392e54d7
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-03-06 11:17:00 +0900

    initalize project

---
+++ src/test/java/com/fascampus/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fascampus.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
