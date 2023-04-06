bbc876ef340a5c44f593a9a4fa959ed45a211e9e
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-04-06 21:57:35 +0900

    init: 프로젝트 설정

---
+++ src/main/java/com/example/sns/SnsApplication.java
@@ -0,0 +1,15 @@
+package com.example.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.support.DefaultSessionAttributeStore;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApplication.class, args);
+	}
+
+}
