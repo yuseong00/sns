9c2bd8095585219b4e75341830107506c5e03e94
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-04-07 16:24:32 +0900

    feat: jwt 기능 구현
    -기능 구현 후 필터에 추가

--- src/main/java/com/example/sns/config/SecurityConfig.java
+++ src/main/java/com/example/sns/config/SecurityConfig.java
@@ -1,17 +1,27 @@
 package com.example.sns.config;
 
+import com.example.sns.config.filter.JwtTokenFilter;
+import com.example.sns.exception.CustomAuthenticationEntryPoint;
+import com.example.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,9 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                ;
-//                .and()
-//                .exceptionHandling()
-//                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
+        ;
     }
 }
