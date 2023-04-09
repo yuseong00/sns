927cfd8718584b3d7a3548c4b025ad139206e091
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-04-09 20:38:48 +0900

    feat:sse 서비스 기능구현
    sse 서비스 기능구현을 통해 알람개선

--- src/main/java/com/example/sns/controller/UserController.java
+++ src/main/java/com/example/sns/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.sns.controller.response.Response;
 import com.example.sns.model.Alarm;
 import com.example.sns.model.User;
 import com.example.sns.model.UserRole;
+import com.example.sns.service.AlarmService;
 import com.example.sns.service.UserService;
 import com.example.sns.util.ClassUtils;
 import lombok.AllArgsConstructor;
@@ -15,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.sql.Timestamp;
 
@@ -25,6 +27,8 @@ import java.sql.Timestamp;
 public class UserController {
 
     private final UserService userService;
+
+    private final AlarmService alarmService;
 
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
@@ -45,6 +49,13 @@ public class UserController {
     public Response<Page<AlarmResponse>> alarm(Pageable pageable, Authentication authentication) {
         User user = ClassUtils.getSafeCastInstance(authentication.getPrincipal(), User.class);
         return Response.success(userService.alarmList(user.getId(), pageable).map(AlarmResponse::fromAlarm));
+    }
+
+    @GetMapping(value = "/alarm/subscribe")
+    public SseEmitter subscribe(Authentication authentication) {
+        log.info("subscribe");
+        User user = ClassUtils.getSafeCastInstance(authentication.getPrincipal(), User.class);
+        return alarmService.connectNotification(user.getId());
     }
 
 
