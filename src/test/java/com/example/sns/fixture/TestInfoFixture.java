51a132aaf63a32718be0cdd0bd6378f55ef3dbc0
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-04-09 13:15:52 +0900

    refac:test코드 수정

--- src/test/java/com/example/sns/fixture/TestInfoFixture.java
+++ src/test/java/com/example/sns/fixture/TestInfoFixture.java
@@ -14,7 +14,6 @@ public class TestInfoFixture {
         info.setBody("body");
         return info;
     }
-
     @Data
     public static class TestInfo {
         private Integer postId;
@@ -23,6 +22,54 @@ public class TestInfoFixture {
         private String password;
         private String title;
         private String body;
+
+        public void setPostId(Integer postId) {
+            this.postId = postId;
+        }
+
+        public void setUserId(Integer userId) {
+            this.userId = userId;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+
+        public Integer getPostId() {
+            return postId;
+        }
+
+        public Integer getUserId() {
+            return userId;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getBody() {
+            return body;
+        }
     }
 }
 
